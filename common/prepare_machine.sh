6c4be920248bbf7425d5f667f5e4a597c3a00deb
hoekstra.maarten@kpmg.nl <hoekstra.maarten@kpmg.nl>
2016-03-03 18:56:08 +0100

    Fixed issue with wrong username to connect and missing deps for KaveToolbox

--- common/prepare_machine.sh
+++ common/prepare_machine.sh
@@ -9,11 +9,14 @@
 # running the prepare_machines.sh script. 
 ###################################################
 
-if [ -f /root/.ssh/id_rsa.pub ]; then 
+if [ -f id_rsa.pub ]; then 
   mkdir /root/.ssh
   chmod 700 /root/.ssh
+  chown root:root /root/.ssh
   mv id_rsa.pub /root/.ssh/authorized_keys
   chmod 600 /root/.ssh/authorized_keys
+  chown root:root /root/.ssh/authorized_keys
+  
   restorecon -R -v /root/.ssh
 
   NODE=`hostname -s`
@@ -21,7 +24,17 @@ if [ -f /root/.ssh/id_rsa.pub ]; then
   echo "127.0.0.1   $NODE.$DOMAIN $NODE localhost localhost.localdomain localhost4 localhost4.localdomain4" > /etc/hosts
   echo "::1   $NODE.$DOMAIN $NODE localhost localhost.localdomain localhost6 localhost6.localdomain6" >> /etc/hosts
   
+  IPS=`ifconfig | grep "inet\saddr:[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | sed -r "s/.*inet addr:([0-9\.]+).*/\1/g"`
+  for IP in $IPS; do
+    if [ $IP != "127.0.0.1" ]; then
+      echo "$IP   $NODE.$DOMAIN $NODE" >> /etc/hosts
+    fi
+  done
+  
+  # Needed for kavetoolbox
+  yum -y install gcc gcc-c++
   nohup yum -y update &
+  
 else 
   echo "ERROR. no public key found."
 fi
--- common/prepare_machines.sh
+++ common/prepare_machines.sh
@@ -13,10 +13,11 @@ DOMAIN=`hostname -d`
 if [ -f /root/.ssh/id_rsa.pub ]; then 
   for NODE in "$@";
   do
-    scp $DIR/prepare_machine.sh $NODE:
-    scp /root/.ssh/id_rsa.pub $NODE:
-    ssh -t $NODE sudo bash prepare_machine.sh
-    ssh $NODE.$DOMAIN echo "done"
+    scp $DIR/prepare_machine.sh KaveAdmin@$NODE:
+    scp /root/.ssh/id_rsa.pub KaveAdmin@$NODE:
+    ssh -t KaveAdmin@$NODE sudo bash prepare_machine.sh
+    ssh $NODE.$DOMAIN echo "done with $NODE"
+    echo
   done
 else
   echo "ERROR. no public key found."
