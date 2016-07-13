f1071e5c4a03450eacbb0a22677df18708de20dd
rvvincelli <Vincelli.Riccardo@KPMG.nl>
2016-07-13 15:00:20 +0200

    ssh key distribution ad nauseam

--- Automation/setup/bin/distribute_keys.sh
+++ Automation/setup/bin/distribute_keys.sh
@@ -11,10 +11,8 @@ ssh-keygen -f "$key" -t rsa -N ''
 
 echo StrictHostKeyChecking$'\t'no > ~/.ssh/config 
 
-#For some unknown reason a sshpass may fail; let's retry a reasonable number of times
-for _ in `seq 1 10`; do
-    for NODE in $@; do
-	sshpass -p "$PASS" ssh-copy-id -i "$key" root@"$NODE"
-    done
-    sleep 10
+#For some unknown reason a sshpass may fail; let's retry ad nauseam
+for NODE in $@; do
+    until sshpass -p "$PASS" ssh-copy-id -i "$key" root@"$NODE"; do sleep 150; done
 done
+
